6bd5464385b7439c714fb8cc98c6895f15e6b50d
Minakshee Kumari <chilli@Minakshees-MacBook-Pro.local>
2025-09-24 15:32:11 +0400

    initial commit code structure

---
+++ src/main/java/bytecode/rag_chat_storage/RagChatStorageApplication.java
@@ -0,0 +1,13 @@
+package bytecode.rag_chat_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagChatStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagChatStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bytecode/rag_chat_storage/RagChatStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package bytecode.rag_chat_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagChatStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
